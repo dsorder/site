b1adeea546ed0c0c17df7bf5c630688888be7e0e
dsorder <80257819+dsorder@users.noreply.github.com>
2025-02-15 20:39:44 +0100

    dsorder site

--- api/frame.js
+++ api/frame.js
@@ -3,14 +3,17 @@ export default function handler(req, res) {
   res.status(200).json({
     "frames": [
       {
-        "image": "https://dsorder.com/preview-image.jpg", // Cambia esto si quieres
-        "buttons": [
-          {
-            "label": "Visitar Dsorder",
-            "action": "link",
-            "target": "https://dsorder.com"
-          }
-        ]
+        "html": "<iframe src='https://dsorder.com' width='100%' height='100%' frameborder='0'></iframe>"
+      }
+    ]
+  });
+}
+export default function handler(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).json({
+    "frames": [
+      {
+        "html": "<iframe src='https://dsorder.com' width='100%' height='100%' frameborder='0'></iframe>"
       }
     ]
   });
